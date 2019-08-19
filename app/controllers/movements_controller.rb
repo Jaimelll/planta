fae5a0cae89b232c4ee854d9d495e796145ee5f3
AndresSilvaBasauri <andresingenios@gmail.com>
2019-08-19 13:22:59 -0700

    resumen

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -1,4 +1,18 @@
 class MovementsController < InheritedResources::Base
 
-  
+  def despi1(prod)
+   
+     aa=[]
+     bb=[]
+     if Formula.where(product_id:prod).count>0 then
+        Formula.where(product_id:prod).each do |formu| 
+           aa.push(formu.for1)
+           bb.push(formu.cantidad*formu.factor)
+    
+        end #formala each
+     end#despi1 if 
+     return  aa, bb
+  end #despi1 def
+
+
 end
