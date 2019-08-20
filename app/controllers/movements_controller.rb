d78e24861a5e67b97b45869bac4b73041952f5f3
AndresSilvaBasauri <andresingenios@gmail.com>
2019-08-20 14:22:05 -0700

    resumen

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -14,6 +14,43 @@ class MovementsController < InheritedResources::Base
      return  aa, bb
   end #despi1 def
 
+  def despi2(aa,bb)
+      aa1=[]
+      bb1=[]
+
+     aa2=[]
+     bb2=[]
+
+     conta=0
+     while aa.length>conta 
+       aa1,bb1=despi1(aa[conta])
+       aa2=aa2+aa1
+       bb2=bb2+bb1
+       conta=conta+1
+     end
+      return  aa1, bb1
+    end #despi2 def
+
+
+    def despi3(prod)
+
+
+      aa,bb=despi1(prod)
+      aa1,bb1=despi2(aa,bb)
+      aa2=aa+aa1
+      bb2=bb+bb1
+
+      while aa1.length>0
+      aa1,bb1=despi2(aa1,bb1)   
+      aa2=aa2+aa1
+      bb2=bb2+bb1  
+      end
+
+      return  aa2, bb2
+    end #despi3 def
+
+
+
 
 
 
