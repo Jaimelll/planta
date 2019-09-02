95ba0a95089d837f2610066f224b7cc8cd351bb1
AndresSilvaBasauri <andresingenios@gmail.com>
2019-09-02 09:49:11 -0700

    reporte5

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -9,5 +9,6 @@ ActiveAdmin.register_page "Dashboard" do
     li link_to "Matriz",reports_vhoja01_path(format:  "xlsx", :param1=> 2) 
     li link_to "Cortes",reports_vhoja01_path(format:  "xlsx", :param1=> 3) 
     li link_to "Programa",reports_vhoja01_path(format:  "xlsx", :param1=> 4) 
+    li link_to "Compras",reports_vhoja01_path(format:  "xlsx", :param1=> 5) 
     end
 end
